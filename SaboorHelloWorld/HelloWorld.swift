085f5e9007a59519480e459177763d7aea9da782
saboorahm <saboorahmadsatti2@gmail.com>
2022-06-14 20:56:30 +0500

    complete

---
+++ SaboorHelloWorld/HelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.swift
+//  SaboorHelloWorld
+//
+//  Created by Saboor on 14/06/2022.
+//  Copyright © 2022 Saboor. All rights reserved.
+//
+
+import Foundation
+
+public struct HelloWorld {
+    public static func msg(){
+        print("Hello World")
+    }
+}
---
+++ SaboorHelloWorld/SaboorHelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  SaboorHelloWorld.h
+//  SaboorHelloWorld
+//
+//  Created by Saboor on 14/06/2022.
+//  Copyright © 2022 Saboor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SaboorHelloWorld.
+FOUNDATION_EXPORT double SaboorHelloWorldVersionNumber;
+
+//! Project version string for SaboorHelloWorld.
+FOUNDATION_EXPORT const unsigned char SaboorHelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaboorHelloWorld/PublicHeader.h>
+
+
